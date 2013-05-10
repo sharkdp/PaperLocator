903d004bcaba81a62bec65d97caad1b112f38a16
sharkdp <davidpeter@web.de>
2013-05-10 21:38:42 +0200

    Added IOP journals and Chem. Rev.

--- pl.js
+++ pl.js
@@ -67,16 +67,19 @@ function findRef(query) {
 
     // Match against typical [Issue] [Page] strings, like "11, 222" or "11 222"
     // Numbers in brackets are ignored, e.g. "11 (33) 222"
-    var s_issue_and_page = '([0-9]+)(?:[^0-9\\(\\)]+)(?:\\([0-9 ]+\\)(?:[^0-9\\(\\)]+))?([0-9]+)';
+    var s_issue_and_page = '[^0-9a-z]*([0-9]+)(?:[^0-9\\(\\)]+)(?:\\([0-9 ]+\\)(?:[^0-9\\(\\)]+))?([0-9]+)';
 
     var r_doi = new RegExp('\\b(10[.][0-9]{4,}(?:[.][0-9]+)*/(?:(?!["&\'<>])\\S)+)\\b', 'g');
     var r_arXiv = new RegExp('\\barXiv:([0-9]{4,}\\.[0-9]{4,}(?:v[0-9]+)?)\\b', 'ig');
     var r_arXiv_old = new RegExp('\\barXiv:? *(?:[a-z)]* )? *((?:[a-z-]+)/[0-9]+(?:v[0-9]+)?)\\b', 'ig');
-    var r_aps = new RegExp('(?:P(?:hys)?(?:\\.?|ical) *R(?:ev)?(?:\\.?|iew) *([A-Z]|Lett(?:\\.?|ers?)|) *' + s_issue_and_page + ')\\b', 'ig');
-    var r_rmp = new RegExp('R(?:ev)?(?:iew)?\\.? *(?:of)? *M(?:od)?(?:ern)?\\.? *P(?:hys)?(?:ics)?\\.? *' + s_issue_and_page + '\\b', 'ig');
-    var r_nature = new RegExp('(?:Nature *(Phys(?:ics)?\\.?)? *(?:\\([^\\)]+\\))? ?' + s_issue_and_page + ')\\b', 'ig');
-    var r_science = new RegExp('(?:Science *(?:\\([^\\)]+\\))? ?' + s_issue_and_page + ')\\b', 'ig');
-    var r_njp = new RegExp('N(?:ew)?\\.? *J(?:ournal)?\\.? *(?:of)? *P(?:hys)?(?:ics)?\\.? *' + s_issue_and_page + '\\b', 'ig');
+    var r_aps = new RegExp('\\b(?:P(?:hys)?(?:\\.?|ical) *R(?:ev)?(?:\\.?|iew) *([A-Z]|Lett(?:\\.?|ers?)|)' + s_issue_and_page + ')\\b', 'ig');
+    var r_rmp = new RegExp('\\bR(?:ev)?(?:iew)?\\.? *(?:of)? *M(?:od)?(?:ern)?\\.? *P(?:hys)?(?:ics)?\\.?' + s_issue_and_page + '\\b', 'ig');
+    var r_nature = new RegExp('\\b(?:Nature *(Phys(?:ics)?\\.?)? *(?:\\([^\\)]+\\))?' + s_issue_and_page + ')\\b', 'ig');
+    var r_science = new RegExp('\\b(?:Science *(?:\\([^\\)]+\\))?' + s_issue_and_page + ')\\b', 'ig');
+    var r_jphys = new RegExp('\\bJ(?:ournal)?\\.? *(?:of)? *P(?:hys)?(?:ics)?[ \\.:]*([ABDG]|C(?:ond(?:ens(?:ed)?)?)[ \\.]*(?:Mat(?:ter)))[^a-z][^0-9]*' + s_issue_and_page + '\\b', 'ig');
+    var r_njp = new RegExp('\\b(N)(?:ew)?\\.? *J(?:ournal)?\\.? *(?:of)? *P(?:hys)?(?:ics)?\\.?' + s_issue_and_page + '\\b', 'ig');
+    var r_repprogphys = new RegExp('\\b(R)ep(?:orts?)?[\\. ]*(?:on)?[\\. ]*Prog(?:ress)?[\\. ]*(?:in)? *Phys(?:ics)?' + s_issue_and_page + '\\b', 'ig');
+    var r_chemrev = new RegExp('\\bChem(?:\\.?|ical)?\\.? *R(?:ev)?(?:\\.?|iews?)' + s_issue_and_page + '\\b', 'ig');
 
     var m, res = '';
 
@@ -159,14 +162,73 @@ function findRef(query) {
         return true;
     }
 
-    // Search for NJP / JP* references
-    if (m = r_njp.exec(query)) {
+    // Search for IOP references
+    if ((m = r_jphys.exec(query)) ||     // Journal of Physics *
+        (m = r_njp.exec(query)) ||       // New Journal of Physics
+        (m = r_repprogphys.exec(query))  // Reports on Progress in Physics
+            ) {
+
+        var journal = m[1].toUpperCase();
+        var volume = m[2];
+        var article = m[3];
+
+        if (journal[0] == 'C') {
+            journal = 'C';
+        }
+
+        var journalName = 'Journal of Physics ' + journal;
+        var shortName = 'J. Phys. ' + journal;
+        if (journal == 'N') {
+            journalName = 'New Journal of Physics';
+            shortName = 'NJP';
+        }
+        else if (journal == 'R') {
+            shortName = journalName = 'Rep. Prog. Phys.';
+        }
+
+        // The IOP journals change their names all the time.
+        // We have to find the right journal ID by comparing
+        // the іssue of the reference to the following small
+        // database of the form
+        // 'Journal name' : {{volume, 'id'}, ...}
+        // where 'volume' is the highest volume number with the
+        // corresponding journal ID. The current ID is listed
+        // with volume set to 0.
+        var iopJournals = {
+            'A' : {5: '0022-3689', 7: '0301-0015', 39: '0305-4470', 0: '1751-8121'},
+            'B' : {20: '0022-3700', 0: '0953-4075'},
+            'C' : {0: '0953-8984'}, // Condensed Matter
+            'D' : {18: '0508-3443', 0: '0022-3727'},
+            'G' : {14: '0305-4616', 0: '0954-3899'},
+            'R' : {0: '0034-4885'}, // Reports on Progress in Physics
+            'N' : {0: '1367-2630'} // New Journal of Physics
+        };
+
+        if (journal in iopJournals) {
+            var ids = iopJournals[journal];
+            var journalID = ids[0];
+            var volumen = parseInt(volume);
+            for (var maxVol in ids) {
+                if (volumen <= maxVol) {
+                    journalID = ids[maxVol];
+                    break;
+                }
+            }
+            record["journal"] = journalName;
+            record["reference"] = shortName + ' <b>' + volume + '</b>, ' + article;
+            record["website"] = 'http://iopscience.iop.org/findcontent?CF_JOURNAL=' + journalID + '&CF_VOLUME=' + volume + '&CF_ISSUE=&CF_PAGE=' + article + '&submit=Go&navsubmit=Go';
+            return true;
+        }
+    }
+
+    // Search for Chemical Review references
+    if (m = r_chemrev.exec(query)) {
         var volume = m[1];
         var article = m[2];
 
-        record["journal"] = 'New Journal of Physics';
-        record["reference"] = 'NJP <b>' + volume + '</b>, ' + article;
-        record["website"] = 'http://iopscience.iop.org/findcontent?CF_JOURNAL=1367-2630&CF_VOLUME=' + volume + '&CF_ISSUE=&CF_PAGE=' + article + '&submit=Go&navsubmit=Go';
+        record["journal"] = 'Chemical Review';
+        record["reference"] = 'Chem. Rev. <b>' + volume + '</b>, ' + article;
+        record["website"] = 'http://pubs.acs.org/action/quickLink?quickLinkJournal=chreay&quickLinkVolume=' + volume + '&quickLinkPage=' + article;
         return true;
     }
 
